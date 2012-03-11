0bdf30f77a3606f67406d977319a6fbc7a513101
Kazunori Sakamoto <exkazuu@gmail.com>
2012-03-11 23:54:13 +0900

    Add extension methods for FileSystemInfos.

---
+++ Paraiba.Core.Tests/IO/FileSystemInfoExtensionsTest.cs
@@ -0,0 +1,80 @@
+﻿#region License
+
+// Copyright (C) 2012 Kazunori Sakamoto
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using System.IO;
+using NUnit.Framework;
+using Paraiba.IO;
+
+namespace Paraiba.Tests.IO {
+	/// <summary>
+	///   Tests for <see cref="FileSystemInfoExtensions" /> .
+	/// </summary>
+	[TestFixture]
+	public class FileSystemInfoExtensionsTest {
+		[Test]
+		public void SafeExistsForFileInfo() {
+			Assert.That(((FileInfo)null).SafeExists(), Is.False);
+			Assert.That(new FileInfo(@"C:\").SafeExists(), Is.False);
+			Assert.That(new FileInfo(@"C:\").SafeFileExists(), Is.False);
+			Assert.That(new FileInfo(@"C:\").SafeDirectoryExists(), Is.False);
+			Assert.That(new FileInfo(@"C:\Windows\notepad.exe").SafeExists(), Is.True);
+			Assert.That(new FileInfo(@"C:\Windows\notepad.exe").SafeFileExists(), Is.True);
+			Assert.That(new FileInfo(@"C:\Windows\notepad.exe").SafeDirectoryExists(), Is.False);
+		}
+
+		[Test]
+		public void SafeExistsForDirectoryInfo() {
+			Assert.That(((DirectoryInfo)null).SafeExists(), Is.False);
+			Assert.That(new DirectoryInfo(@"C:\").SafeExists(), Is.True);
+			Assert.That(new DirectoryInfo(@"C:\").SafeFileExists(), Is.False);
+			Assert.That(new DirectoryInfo(@"C:\").SafeDirectoryExists(), Is.True);
+			Assert.That(new DirectoryInfo(@"C:\Windows\notepad.exe").SafeExists(), Is.False);
+			Assert.That(new DirectoryInfo(@"C:\Windows\notepad.exe").SafeFileExists(), Is.False);
+			Assert.That(new DirectoryInfo(@"C:\Windows\notepad.exe").SafeDirectoryExists(), Is.False);
+		}
+
+		[Test]
+		public void SafeExistsForFileSystemInfo() {
+			Assert.That(((FileSystemInfo)null).SafeExists(), Is.False);
+			Assert.That(((FileSystemInfo)new FileInfo(@"C:\")).SafeExists(), Is.False);
+			Assert.That(((FileSystemInfo)new FileInfo(@"C:\")).SafeFileExists(), Is.False);
+			Assert.That(((FileSystemInfo)new FileInfo(@"C:\")).SafeDirectoryExists(), Is.False);
+			Assert.That(((FileSystemInfo)new FileInfo(@"C:\Windows\notepad.exe")).SafeExists(), Is.True);
+			Assert.That(((FileSystemInfo)new FileInfo(@"C:\Windows\notepad.exe")).SafeFileExists(), Is.True);
+			Assert.That(((FileSystemInfo)new FileInfo(@"C:\Windows\notepad.exe")).SafeDirectoryExists(), Is.False);
+			Assert.That(((FileSystemInfo)new DirectoryInfo(@"C:\")).SafeExists(), Is.True);
+			Assert.That(((FileSystemInfo)new DirectoryInfo(@"C:\")).SafeFileExists(), Is.False);
+			Assert.That(((FileSystemInfo)new DirectoryInfo(@"C:\")).SafeDirectoryExists(), Is.True);
+			Assert.That(((FileSystemInfo)new DirectoryInfo(@"C:\Windows\notepad.exe")).SafeExists(), Is.False);
+			Assert.That(((FileSystemInfo)new DirectoryInfo(@"C:\Windows\notepad.exe")).SafeFileExists(), Is.False);
+			Assert.That(((FileSystemInfo)new DirectoryInfo(@"C:\Windows\notepad.exe")).SafeDirectoryExists(), Is.False);
+		}
+
+		[Test]
+		public void GetFile() {
+			var info = new DirectoryInfo("C:/test").GetFile("test.txt");
+			Assert.That(info.FullName, Is.EqualTo("C:\\test\\test.txt"));
+		}
+
+		[Test]
+		public void GetDirectory() {
+			var info = new DirectoryInfo("C:/test").GetDirectory("test");
+			Assert.That(info.FullName, Is.EqualTo("C:\\test\\test"));
+		}
+	}
+}
---
+++ Paraiba.Core/IO/FileSystemInfoExtensions.cs
@@ -0,0 +1,64 @@
+﻿#region License
+
+// Copyright (C) 2012 Kazunori Sakamoto
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using System.IO;
+
+namespace Paraiba.IO {
+	public static class FileSystemInfoExtensions {
+		/// <summary>
+		///   Returns a value indicating whether the file or directory exists null-safely.
+		/// </summary>
+		/// <param name="info"> the </param>
+		/// <returns> </returns>
+		public static bool SafeExists(this FileSystemInfo info) {
+			return info != null && info.Exists;
+		}
+
+		public static bool SafeFileExists(this FileSystemInfo info) {
+			return info.SafeExists() && (info.Attributes & FileAttributes.Directory) == 0;
+		}
+
+		public static bool SafeFileExists(this FileInfo info) {
+			return info.SafeExists();
+		}
+
+		public static bool SafeFileExists(this DirectoryInfo info) {
+			return false;
+		}
+
+		public static bool SafeDirectoryExists(this FileSystemInfo info) {
+			return info.SafeExists() && (info.Attributes & FileAttributes.Directory) != 0;
+		}
+
+		public static bool SafeDirectoryExists(this DirectoryInfo info) {
+			return info.SafeExists();
+		}
+
+		public static bool SafeDirectoryExists(this FileInfo info) {
+			return false;
+		}
+
+		public static FileInfo GetFile(this DirectoryInfo info, string path) {
+			return new FileInfo(Path.Combine(info.FullName, path));
+		}
+
+		public static DirectoryInfo GetDirectory(this DirectoryInfo info, string path) {
+			return new DirectoryInfo(Path.Combine(info.FullName, path));
+		}
+	}
+}
