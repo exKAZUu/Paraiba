c56a27efc74110410ca2b4df15270206973355ae
Kazunori Sakamoto <exkazuu@gmail.com>
2012-10-19 10:21:31 +0900

    Change project settings for travis-ci.

--- Paraiba.Core/Properties/AssemblyInfo.cs
+++ Paraiba.Core/Properties/AssemblyInfo.cs
@@ -54,5 +54,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.3.58")]
-[assembly: AssemblyFileVersion("1.0.3.58")]
+[assembly: AssemblyVersion("1.0.3.59")]
+[assembly: AssemblyFileVersion("1.0.3.59")]
--- Paraiba.Experiment/Properties/AssemblyInfo.cs
+++ Paraiba.Experiment/Properties/AssemblyInfo.cs
@@ -55,5 +55,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.2.34")]
-[assembly: AssemblyFileVersion("1.0.2.34")]
+[assembly: AssemblyVersion("1.0.2.37")]
+[assembly: AssemblyFileVersion("1.0.2.37")]
