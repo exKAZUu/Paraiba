1223791c4b7c9f74d3e07c758bdf73f3ca916e50
Kazunori Sakamoto <exkazuu@gmail.com>
2012-12-04 18:53:42 +0900

    Add new class for enhancing Environment. Update dependencies.

---
+++ Paraiba.Core/Core/EnvironmentExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Paraiba.Core
+{
+	/// <summary>
+	/// A utility class for enhancing <see cref="Environment" />.
+	/// </summary>
+	public static class EnvironmentUtils
+	{
+		/// <summary>
+		/// Returns the boolean whether this program runs on Mono or not.
+		/// </summary>
+		/// <returns>The boolean whether this program runs on Mono or not.</returns>
+		public static bool OnMono() {
+			int p = (int) Environment.OSVersion.Platform;
+			return (p == 4) || (p == 6) || (p == 128);
+		}
+	}
+}
