1a74ca07c3b22b74f912a47ef1177b6f064d06c3
Kazunori Sakamoto <exkazuu@gmail.com>
2012-12-07 12:15:35 +0900

    Bump versions and fix a little.

--- Paraiba.Core/Properties/AssemblyInfo.cs
+++ Paraiba.Core/Properties/AssemblyInfo.cs
@@ -54,5 +54,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.4.64")]
-[assembly: AssemblyFileVersion("1.0.4.64")]
+[assembly: AssemblyVersion("1.0.4.65")]
+[assembly: AssemblyFileVersion("1.0.4.65")]
