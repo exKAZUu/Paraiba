4720944c021da5beb4a402befc787a4aeb574b29
Kazunori Sakamoto <exkazuu@gmail.com>
2013-01-25 23:29:32 +0900

    Fix build script for Windows.

--- Paraiba.Experiment/Properties/AssemblyInfo.cs
+++ Paraiba.Experiment/Properties/AssemblyInfo.cs
@@ -55,5 +55,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.2.39")]
-[assembly: AssemblyFileVersion("1.0.2.39")]
+[assembly: AssemblyVersion("1.0.2.40")]
+[assembly: AssemblyFileVersion("1.0.2.40")]
